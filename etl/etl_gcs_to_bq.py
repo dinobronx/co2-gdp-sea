146e6894ad9590d203d7f5a7f7925a9666839497
Dianne Bronola <dbronola@gmail.com>
2023-04-04 00:55:24 -0400

    format econ indicator, add schema for big query

--- etl/etl_gcs_to_bq.py
+++ etl/etl_gcs_to_bq.py
@@ -16,37 +16,82 @@ def prep_temp():
 def extract_from_gcs() -> Path:
     gcs_block = GcsBucket.load('econ-bucket-creds')
     gcs_path = 'de-zoomcamp/economic-analysis.parquet'
-    local_path = f'{temp_path}/{gcs_file}'
-    gcs_block.get_directory(from_path=gcs_path, local_path=local_path)
-    return Path(local_path)
+    gcs_block.get_directory(from_path=gcs_path)
+    return Path(gcs_path)
 
+def clean_econ_indicator(indicator: str) -> str:
+    """ Rename economic indicator for friendly query """
+    if indicator ==  'CO2 emissions (kt)':
+        return 'emissions'
+    elif indicator == 'GDP (current US$)':
+        return 'gdp_real'
+    elif indicator == 'GDP growth (annual %)':
+        return 'gdp_growth'
+    elif indicator == 'GDP per capita (current US$)':
+        return 'gdp_percapita'
+    elif indicator == 'Net migration':
+        return 'net_migration'
+    elif indicator == 'Population ages 0-14 (% of total population)':
+        return 'population_young'
+    elif indicator == 'Population ages 15-64 (% of total population)':
+        return 'population_working'
+    elif indicator == 'Population ages 65 and above (% of total population)':
+        return 'population_old'
+    elif indicator == 'Population, total':
+        return 'population_total'
+    elif indicator == 'Unemployment, total (% of total labor force) (national estimate)':
+        return 'unemployment_rate'
+    return None
 @task()
-def read(gcpath: Path) -> pd.DataFrame:
+def transform(gcpath: Path) -> pd.DataFrame:
     """ Data cleaning sample """
     df = pd.read_parquet(gcpath)
+    
+    # rename column to remove space
+    df.rename(columns={'Country Name': 'country', 
+                       'Country Code': 'country_code',
+                       'Indicator Name': 'econ_indicator', 
+                       'Year': 'year', 
+                       'Value': 'value'}, inplace=True)
     # make sure we have no nulls
+    
+    df['econ_indicator'] = df['econ_indicator'].apply(clean_econ_indicator)
+    
     print('***** Finding null data *****')
     print(df.isnull().sum(axis=0))
+    print(f'length of downloaded data: {len(df)}')
+    # if os.path.exists(gcpath):
+    #   os.rmdir(gcpath)
+    
     return df
 
 @task()
 def write_bq(df: pd.DataFrame) -> None:
     """ Write data in Big Query """
     gcp_credentials = GcpCredentials.load("zoom-gcp-creds")
+    schema = [
+        {'name':'country', 'type': 'STRING'},
+        {'name':'country_code', 'type': 'STRING'},
+        {'name':'econ_indicator', 'type': 'STRING'},
+        {'name':'year', 'type': 'INTEGER'},
+        {'name':'value', 'type': 'FLOAT64'}
+    ]
     df.to_gbq(
         destination_table='sea_economic_analysis.rawdata',
         project_id='alien-handler-376020',
         credentials=gcp_credentials.get_credentials_from_service_account(),
         chunksize=500_000,
-        if_exists="replace"
+        if_exists="replace",
+        table_schema=schema
     )
+    
 
 @flow()
 def etl_gcs_to_bq() -> None:
     """ Main etl flow to load data to Big Query"""
     prep_temp()
     gcpath = extract_from_gcs()
-    df = read(gcpath)
+    df = transform(gcpath)
     write_bq(df)
 
 if __name__ == '__main__':
